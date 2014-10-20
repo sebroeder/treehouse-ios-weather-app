cb5892cb7a5857e00114dc5b517ea73f7c3831e5
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-10-20 19:58:22 +0200

    Initial commit

---
+++ Stormy/Stormy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Sebastian Röder on 20/10/14.
+//  Copyright (c) 2014 Sebastian Röder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
