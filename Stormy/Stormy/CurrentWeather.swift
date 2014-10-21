da3d37ffc37122af825d95a3fe3007757d0ec841
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-10-21 23:30:10 +0200

    Use a read-only computed property for timeString

--- Stormy/Stormy/CurrentWeather.swift
+++ Stormy/Stormy/CurrentWeather.swift
@@ -10,29 +10,30 @@ import Foundation
 
 struct CurrentWeather {
 
-    let currentTime: String?
+    let currentTime: Int
     let temperature: Int
     let humidity: Double
     let precipitationProbability: Double
     let summary: String
     let iconName: String
 
+    var timeString: String {
+        let weatherDate = NSDate(timeIntervalSince1970: NSTimeInterval(currentTime))
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = .ShortStyle
+        return dateFormatter.stringFromDate(weatherDate)
+    }
+
     init(weatherJSON: NSDictionary) {
         let current = weatherJSON["currently"] as NSDictionary
 
+        currentTime = current["time"] as Int
         temperature = current["temperature"] as Int
         humidity = current["humidity"] as Double
         precipitationProbability = current["precipProbability"] as Double
         summary = current["summary"] as String
         iconName = current["icon"] as String
-
-        currentTime = dataStringWithUnixTime(current["time"] as Int)
     }
 
-    private func dataStringWithUnixTime(unixTime: Int) -> String {
-        let weatherDate = NSDate(timeIntervalSince1970: NSTimeInterval(unixTime))
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.timeStyle = .ShortStyle
-        return dateFormatter.stringFromDate(weatherDate)
-    }
 }
+
